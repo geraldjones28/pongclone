8df52f9c49650416df4e7e1514f7403b77341f5f
Gerald <gajone12@asu.edu>
2024-02-26 16:30:37 -0800

    feat: Create TextMesh to keep track of score and implement ScoreText script to handle score incrementation

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreText : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    public void SetScore(int value) {
+        text.text = value.ToString();
+    }
+}
