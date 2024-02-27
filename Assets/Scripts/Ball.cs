3b1f604c27ba9b73b4548a2e7f92054a99928604
Gerald <gajone12@asu.edu>
2024-02-26 16:16:03 -0800

    feat: Proper implementation of ball resetting within ScoreZones; resets left or right randomly with Random.value function

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,21 +8,32 @@ public class Ball : MonoBehaviour
     public Rigidbody2D rb2d;
     public float maxInitialAngle = 0.67f;
     public float moveSpeed = 1f;
+    public float maxStartY = 4f;
+
+    private float startX = 0f;
 
     private void Start() {
         InitialPush();
     }
 
     private void InitialPush() {
-        Vector2 dir = Vector2.right;
+        Vector2 dir = Random.value < 0.5f ? Vector2.left : Vector2.right;
+
         dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
         rb2d.velocity = dir * moveSpeed;
+    }
+
+    private void ResetBall() {
+        float posY = Random.Range(-maxStartY, maxStartY);
+        Vector2 position = new Vector2(startX, posY);
+        transform.position = position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         ScoreZone scoreZone = collision.GetComponent<ScoreZone>();
         if (scoreZone) {
-            Debug.Log("do something!");
+            ResetBall();
+            InitialPush();
         }
     }
 }
